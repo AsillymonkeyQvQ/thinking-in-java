b6368fd15721f0abbd3037d0a38aee029bdbe405
sunzehai <zehai.sun@icloud.com>
2021-09-24 12:14:05 +0900

    object

---
+++ src/object/Documentation2.java
@@ -0,0 +1,11 @@
+//: object/Documentation1.java
+package object;
+
+/**
+ * <pre>
+ *     System.out.println(new Date());
+ * </pre>
+ */
+public class Documentation2 {
+
+}
---
+++ src/object/Documentation3.java
@@ -0,0 +1,14 @@
+//: object/Documentation3.java
+package object;
+
+/**
+ * You can <em>even</em> insert a list:
+ * <ol>
+ *     <li>Item one</li>
+ *     <li>Item two</li>
+ *     <li>Item three</li>
+ * </ol>
+ */
+public class Documentation3 {
+
+}
