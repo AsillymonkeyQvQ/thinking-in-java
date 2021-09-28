3eff7cedb4b68ed91067b5acbdb9fd7150643109
sunzehai <zehai.sun@icloud.com>
2021-09-28 22:19:22 +0900

    reusing

---
+++ src/reusing/Lisa.java
@@ -0,0 +1,10 @@
+//: reusing/Lisa.java
+// {CompileTimeError} (Won't compile)
+package reusing;
+
+public class Lisa {
+    // @Override
+    void doh(Milhouse m) {
+        System.out.println("doh(Milhouse)");
+    }
+}
