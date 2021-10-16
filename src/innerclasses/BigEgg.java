704ef0a1fcb3baeb2c07fdfb7565a3fd92366582
sunzehai <zehai.sun@icloud.com>
2021-10-16 17:36:55 +0900

    innerclasses

---
+++ src/innerclasses/BigEgg.java
@@ -0,0 +1,25 @@
+// An inner class cannot be overriden like a method.
+package innerclasses;
+
+import static net.mindview.util.Print.print;
+
+class Egg {
+    private Yolk y;
+    protected class Yolk {
+        public Yolk() { print("Egg.Yolk()"); }
+    }
+    public Egg() {
+        print("New Egg()");
+        y = new Yolk();
+    }
+}
+
+public class BigEgg extends Egg {
+    public class Yolk {
+        public Yolk() { print("BigEgg.Yolk()"); }
+    }
+
+    public static void main(String[] args) {
+        new BigEgg();
+    }
+}
